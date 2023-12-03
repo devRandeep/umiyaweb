ac2837630abb018b9a61473886950b0fc7082bc2
Rajat Ahuja <rajat.ahuja@eglogics.com>
2023-12-03 21:32:54 +0530

    03-12-23

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php include 'header.php';?>
+
+<section class="banner_section background up-scroll" id="banner_section">
+    <div class="content-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                <div class="slideText">
+                    <h1 class="heading_section_one">Sip, Refresh <br> Energise</h1>
+                    <p class="text_section_one">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
+                </div>
+            </div>
+            <div class="col-md-8 p-0" style="position: relative;">
+                 <div class="prodcutWrap">
+                    <div class="women"><img src="images/women.png" alt=""></div>
+                    <div class="lyr"></div>
+                        <div class="img1"><img src="images/img_19.png" alt=""></div>
+                </div>
+            </div>              
+            </div>
+        </div>
+        
+    <img src="images/leaf1.png" class="leaf leaf1" alt="">
+    <img src="images/leaf2.png" class="leaf leaf2" alt="">
+    <img src="images/leaf3.png" class="leaf leaf3" alt="">
+    <img src="images/leaf4.png" class="leaf leaf4" alt="" style="width: 40px;">
+    </div>
+</section>
+
+<section class="choices_make_diffrence background" id="choices">
+    <div class="content-wrapper">
+         <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                <div class="slideText">
+                    <h1 class="heading_section_one">A Tea-verse<br> Curated For You</h1>
+                    <p class="text_section_one">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
+                </div>
+            </div>
+            <div class="col-md-8 p-0" style="position: relative;">
+                 <div class="prodcutWrap">
+                    <div class="women"><img src="images/women2.png" alt=""></div>
+                        <div class="img1"><img src="images/img_10.png" alt=""></div>
+                </div>
+            </div>              
+            </div>
+        </div>
+        <img src="images/leaf1.png" class="leaf leaf1" alt="">
+        <img src="images/leaf2.png" class="leaf leaf2" alt="">
+        <img src="images/leaf3.png" class="leaf leaf3" alt="">
+        <img src="images/leaf4.png" class="leaf leaf4" alt="" style="width: 40px;">
+    </div>
+</section>
+<!-- 
+<section class="weare_chnaging background" id="weareidtwo">
+    <div class="content-wrapper">
+         <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                <div class="">
+                    <h1>section3</h1>
+                </div>
+            </div>              
+            </div>
+        </div>
+    </div>
+</section> -->
+
+
+
+
+
+
+
+<?php include 'footer.php';?>
